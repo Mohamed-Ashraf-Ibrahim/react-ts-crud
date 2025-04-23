a40085b155d2c98049f2479df337e29fd06e83a0
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2025-04-23 17:08:59 +0200

    feat: Add product form with input handling
    - Added a form to create a new product with fields: title, description, imageURL.
    - Implemented controlled form inputs, binding them to product state.
    - Set up dynamic form rendering based on formInputsList.
    - Integrated modal for adding a new product.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,54 @@
-import { useState } from "react";
-import ProductCard from "./components/ProductCard";
+import { ChangeEvent, useState } from "react";
 import { formInputsList, productList } from "./data";
-import Modal from "./ui/Modal";
-import Button from "./ui/Button";
+import { IProduct } from "./interfaces";
+
+import ProductCard from "./components/ProductCard";
 import Input from "./ui/Input";
+import Button from "./ui/Button";
+import Modal from "./ui/Modal";
 
 const App = () => {
+  // STATES
   const [isOpen, setIsOpen] = useState(false);
+  const [product, setProduct] = useState<IProduct>({
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
+    colors: [],
+    category: {
+      name: "",
+      imageURL: "",
+    },
+  });
 
-  function openModal() {
-    setIsOpen(true);
-  }
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setProduct({
+      ...product,
+      [name]: value,
+    });
+  };
 
   // RENDER
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
   const renderFormList = formInputsList.map((input) => (
-    <div className="flex flex-col">
-      <label htmlFor={input.id} className="text-lg font-semibold">{input.label}</label>
-      <Input type="text" key={input.id} id={input.id} name={input.name} />
+    <div key={input.id} className="flex flex-col">
+      <label htmlFor={input.id} className="text-sm font-semibold">
+        {input.label}
+      </label>
+      <Input
+        type="text"
+        id={input.id}
+        name={input.name}
+        value={product[input.name]}
+        onChange={onChangeHandler}
+      />
     </div>
   ));
 
@@ -37,11 +62,11 @@ const App = () => {
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="ADD A NEW Product">
         <form action="">
+          
           {renderFormList}
           <div className="flex items-center space-x-3">
             <Button
               color="bg-indigo-700 hover:bg-indigo-800"
-              onClick={closeModal}
             >
               Cancel
             </Button>
